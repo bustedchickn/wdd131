c7386ab92b8bcc4947528876f44d6207085356c9
bustedchickn <talmage.kay@gmail.com>
2025-10-19 22:32:30 -0600

    ahhhhhhhhhhhhh

---
+++ blog/blog.js
@@ -0,0 +1,48 @@
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '****'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	}
+]
+
+articles.forEach(book => {
+    const article = document.createElement("article");
+    article.classList.add('book');
+
+    article.innerHTML = `
+    <aside class="book-info">
+          <p class="date">${book.date}</p>
+          <p class="ages">${book.ages}</p>
+          <p class="genre">${book.genre}</p>
+          <p class="stars">${book.stars}</p>
+        </aside>
+
+        <section class="book-view">
+          <h2 class="title">${book.title}</h2>
+          <img src="${book.imgSrc}" alt="${book.imgAlt}" />
+          <p class="description">${book.description} <a href="#">Read More</a></p>
+        </section>
+    `
+    document.getElementById('book-list').appendChild(article);
+});
